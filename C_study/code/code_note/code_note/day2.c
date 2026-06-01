e0d13f17afd7e7a6f0442fcf17e5167edb553578
C1-M <2924382572@qq.com>
2026-06-01 16:24:25 +0800

    day_1

---
+++ C_study/code/code_note/code_note/day2.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+
+//转义字符
+//#include<stdio.h>
+//int main() {
+//	printf("%c\n", '\'');
+//}
+
+//#include<stdio.h>
+//int main() {
+//	printf("%s\n", "abcdef");
+//	printf("%s\n", "x");
+//	printf("%s\n", "\"");
+//	printf("%s\n", "''''");
+//	return 0;
+//}
---
+++ C_study/code/code_note/code_note/test.h
@@ -0,0 +1,3 @@
+#pragma once
+//函数声名
+//int add(int x, int y);
